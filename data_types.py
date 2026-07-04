d0fff0672b0a7b78e6fdf5d4f1ab3ec47d5289fb
Сутормин Михаил Александрович <m.sutormin@corp.nanotronica.ru>
2026-07-05 01:13:53 +0300

    Jul_5t

---
+++ data_types.py
@@ -0,0 +1,8 @@
+10 is int
+10.5 is float
+"hello" is str
+True or False is bool
+[1, 2, 3] is list
+(1, 2, 3) is tuple
+{1, 2, 3} is set
+{'a': 1, 'b': 2} is dict
---
+++ operators.py
@@ -0,0 +1,33 @@
+# Arithmetic:
+a = 1 + 1
+b = 1 - 1
+c = 1 * 1
+d = 2 / 5
+e = 2 % 2
+f = 2 ** 2
+g = 2 // 1.5
+
+print(a, b, c, d, e, f, g)
+
+
+# Comparison:
+a == b
+b != c
+c > d
+d < e
+e >= f
+f <= g
+
+
+# Logical:
+a and b or c 
+not d
+
+
+# Assignment:
+a = b
+b += c
+c -= d
+d *= e
+e /= f
+f %= g
